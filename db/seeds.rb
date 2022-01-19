d33fa8e4882e9b5d8096afa70c6a4d1ca1e313c6
shaywacker <shay2442@yahoo.com>
2022-01-19 10:55:24 -0600

    commit tree 298fef26deee5c430c8ea1f804452bef3dde1470

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,10 @@ puts "🌱 Seeding spices..."
 # Seed your database here
 
 shay= User.create(name: "Shay")
+dad = User.create(name: "Dad")
 
 shay.recipes.create(title: "Enchiladas", from: "Mom",  category:"Main Dish", notes:"Yummy!")
 shay.recipes.create(title: "Nachos", from: "Friend",  category:"Side Dish", notes:"Very cheesy!")
 shay.recipes.create(title: "Chocolate Cake", from: "Grammy",  category:"Dessert", notes:"The best cake ever!")
+dad.recipes.create(title:"Bacon Poppers", from:"Dad", notes:"Don't bake too long", category:"Appetizer")
 puts "✅ Done seeding!"
